f3b4b7e0fc0d9a471e7e5479dec1e4fe85506b50
AbdulrahmanAlosaimi <osaimi-a@hotmail.com>
2023-01-14 00:16:57 +0300

    Added error display

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { NgForm, Validators } from '@angular/forms';
 import { User } from 'src/app/models/User';
 
 @Component({
@@ -7,6 +15,8 @@ import { User } from 'src/app/models/User';
   styleUrls: ['./user-form.component.css'],
 })
 export class UserFormComponent implements OnInit {
+  numberRegex: RegExp = /^[0-9]\d*$/;
+  @ViewChild('form') form: NgForm | any;
   @Input() user: User = new User();
 
   @Output() addOrder = new EventEmitter<User>();
@@ -14,4 +24,18 @@ export class UserFormComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  validateCreditCard(value: any) {
+    this.form.controls['creditCard'].setErrors({
+      pattern: this.numberRegex.test(this.user.creditCard),
+      required: !this.user.creditCard,
+    });
+    if (
+      this.numberRegex.test(this.user.creditCard) &&
+      this.user.creditCard &&
+      this.user.creditCard.length == 16
+    ) {
+      this.form.controls['creditCard'].setErrors(null);
+    }
+  }
 }
